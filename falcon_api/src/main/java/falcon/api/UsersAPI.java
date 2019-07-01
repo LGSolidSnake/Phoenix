c8bc77658729062113e6cf74bd9390f2878fb0eb
LGSolidSnake <lgsolidsnake@googlemail.com>
2019-07-01 20:45:59 +0100

    Added swagger bean.

--- falcon_api/src/main/java/falcon/api/UsersAPI.java
+++ falcon_api/src/main/java/falcon/api/UsersAPI.java
@@ -2,12 +2,13 @@ package falcon.api;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/users")
 public interface UsersAPI
 {
-
-    public ResponseEntity<String> addUser(String name);
+    @RequestMapping(value = "/", method = RequestMethod.POST)
+    ResponseEntity<String> addUser(String name);
 }
--- falcon_controller/src/main/java/falcon/controller/UsersController.java
+++ falcon_controller/src/main/java/falcon/controller/UsersController.java
@@ -3,10 +3,13 @@ package falcon.controller;
 import falcon.api.UsersAPI;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class UsersController implements UsersAPI
 {
-    public ResponseEntity<String> addUser(String name)
+    public ResponseEntity<String> addUser(@RequestBody String name)
     {
         return new ResponseEntity<String>(HttpStatus.OK);
     }
